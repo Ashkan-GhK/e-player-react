ca83b9bb1051db4d9077f7b7955b7e0422a86626
Alex <ashkan.ghk84@gmail.com>
2020-08-15 10:15:38 +0100

    info Card

---
+++ e-player-react/src/components/MusicInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/MusicInfo.css';
+
+const MusicInfo = () => {
+    return (
+        <div className="musicInfo_container">
+            <h3>The name of Song</h3>
+            <h5>Singer name</h5>
+        </div>
+    )
+}
+
+export default MusicInfo
