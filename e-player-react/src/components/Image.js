f7dac682c6fe7550a89a70ac4e948392d150fa12
Alex <ashkan.ghk84@gmail.com>
2020-08-15 10:14:17 +0100

    Song Image

---
+++ e-player-react/src/components/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Image.css';
+
+const Image = () => {
+    return (
+        <div>
+           <img className="image" src="https://d1eqqkloubk286.cloudfront.net/static/mp3/ali-azimi-panahandeh/f3b91f524109872.jpg" alt="music" /> 
+        </div>
+    )
+}
+
+export default Image;
