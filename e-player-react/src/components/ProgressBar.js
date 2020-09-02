4d70fd674cd830a1af45dd4c2fbab5052320a9d8
Alex <ashkan.ghk84@gmail.com>
2020-09-02 13:25:25 +0100

    progress bar added to audio player

---
+++ e-player-react/src/components/ProgressBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/ProgressBar.css';
+
+const ProgressBar = () => {
+    return (
+        <div className="progressBar-container">
+             <div className="progressBar">
+                 <div className="progressBar-circle"></div>
+
+             </div>
+        </div>
+    )
+}
+
+export default ProgressBar
